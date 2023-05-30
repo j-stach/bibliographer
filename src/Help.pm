8e2c547c332b187b304689e87c5739e4aab00385
Jordan <jordan.stachura@gmail.com>
2023-05-30 04:34:48 -0400

    updated to reflect changes to commands

--- src/Help.pm
+++ src/Help.pm
@@ -20,14 +20,15 @@ OPTIONS
     -T, --test
     	Run integration testing to ensure the program has been set up correctly.
 
-    -C (or --convert) <filename.rtf> [<new_filename>] [--CITATION_STYLE]
+COMMANDS
+    convert <filename.rtf> [<new_filename>] [--CITATION_STYLE]
     	Converts filename.rtf into the new CITATION_STYLE, overwriting it in the process. 
 	If <new_filename> is provided, the reformatted bibliography will be saved as ./saved_bibs/new_filename.rtf instead.
 	If no CITATION_STYLE is provided, the original file will not be modified,
 	and the raw bibliography info will be saved as ./raw_bibs/filename.raw.txt
         If <new_filename> is provided, the raw bibliography will be saved as ./raw_bibs/new_filename.raw.txt instead.
 
-    -X (or --export) <bibliography_name> [<new_filename>] --CITATION_STYLE
+    export <bibliography_name> [<new_filename>] --CITATION_STYLE
         Exports ./raw_bibs/bibliography_name.raw.txt formatted to CITATION_STYLE as ./saved_bibs/bibliography_file.rtf
         If <new_filename> is provided, it will be exported as ./saved_bibs/new_filename.rtf instead.
 
@@ -35,13 +36,13 @@ CITATION_STYLE
     --MLA (Modern Language Association)
 
 EXAMPLES
-    \$> bibliographer --convert filename.rtf new_file --RAW
+    \$> bibliographer convert filename.rtf new_file
     	This extracts the citation info from filename.rtf and saves it as ./raw_bibs/new_file.raw.txt without formatting the citations.
 
-    \$> bibliographer -C filename.rtf --MLA	 or	bibliographer --MLA --convert filename.rtf
-    	These overwrite filename.rtf in the MLA citation style.
+    \$> bibliographer convert filename.rtf --MLA
+    	This overwrites filename.rtf in the MLA citation style.
 
-    \$> bibliographer -X bibliography --MLA
+    \$> bibliographer export bibliography --MLA
     	This searches ./raw_bibs/ for bibliography.raw.txt and exports it to ./saved_bibs/bibliography.rtf in the MLA citation style.
 \n"; }
 
